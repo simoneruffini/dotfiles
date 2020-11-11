6dbbce9a97009e142e91c92e6e411ed0e82bd6ed
Simone Ruffini <simone.ruffini@tutanota.com>
2020-11-11 19:42:12 +0100

    added powermenus.sh script for beemenu

---
+++ .local/bin/powermenu.sh
@@ -0,0 +1,8 @@
+case "$(echo -e " Exit sway\n🔒 Lock\n⏱ Suspend\n⏼ Hibernate\n⏻ Power Off\n🔄 Reboot\n" | bemenu -l 6 -p "Power:")" in
+" Exit sway") swaymsg exit ;;
+"🔒 Lock") loginctl lock-session ;;
+"⏱ Suspend") systemctl suspend ;;
+"⏼ Hibernate") systemctl hibernate ;;
+"⏻ Power Off") systemctl poweroff ;;
+"🔄 Reboot") systemctl reboot ;;
+esac
