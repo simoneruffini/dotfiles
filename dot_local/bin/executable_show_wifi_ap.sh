18ea15b7f58400cabc7b2d3f883cc9c340247134
Simone Ruffini <simone.ruffini@tutanota.com>
2022-05-07 11:10:05 +0200

    feat: wifi applet connects to prev. created networks

--- dot_local/bin/executable_show_wifi_ap.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-nmcli d wifi | bemenu --list 10 --prompt "wifi" --no-exec --scrollbar "always" --grab --monitor "-1" >/dev/null
--- dot_local/bin/executable_wifi_applet.sh
+++ dot_local/bin/executable_wifi_applet.sh
@@ -1,2 +1,10 @@
 #! /bin/sh
-nmcli d wifi list --rescan auto | bemenu -l 10 -p "wifi" --no-exec --scrollbar "always" --grab --monitor "-1" | grep -P "([0-9A-Z]{2}:){5}[0-9A-Z]{2}" -o | xargs -I{} nmcli d wifi connect {} | xargs -I{} notify-send "wifi connection" {}
+scan="no";
+
+if ! [ -z $1 ]; then
+  if [ $1 = "scan" ] ; then
+    scan="yes";
+  fi
+fi
+
+nmcli d wifi list --rescan $scan | bemenu -l 10 -p "wifi networks" --no-exec --scrollbar "always" --grab --monitor "-1" | grep -P "([0-9A-Z]{2}:){5}[0-9A-Z]{2}" -o | xargs -I{} nmcli d wifi connect {} | xargs -I{} notify-send "wifi connection" {}
