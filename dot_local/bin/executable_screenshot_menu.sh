86f93b4d19dc2f39dfce49fc086ab4f8f2d78fc5
Simone <simone.ruffini@tutanota.com>
2023-02-04 20:10:23 +0100

    feat: screenshot with mod+shif+s

--- dot_local/bin/executable_screenshot_menu.sh
+++ dot_local/bin/executable_screenshot_menu.sh
@@ -14,9 +14,18 @@ else
 	action="$1"
 fi
 
-case "$(echo -e " Select window\n Select Area\n Screen\n All Screens\n" | bemenu -l 4 -m all -p --no-overlap "Screenshot:")" in
-" Select window")	$base_cmd $action window;;  
-" Select Area")	$base_cmd $action area	;;  
-" Screen") 		$base_cmd $action output;;  
-" All Screens") 	$base_cmd $action screen;;  
+print_area=" Select Area"
+print_window=" Select window"
+print_screen=" Screen"
+print_all_screens=" All Screens\n"
+
+bemenu_cmd="bemenu --list 4 --monitor all --no-overlap --prompt Screenshot:"
+
+ret_val=$(echo -e "$print_area\n$print_window\n$print_screen\n$print_all_screens\n" | $bemenu_cmd )
+
+case "$ret_val" in
+"$print_area")	        $base_cmd $action area;;  
+"$print_window")	    $base_cmd $action window;;  
+"$print_screen") 		$base_cmd $action output;;  
+"$print_all_screens") 	$base_cmd $action screen;;  
 esac
